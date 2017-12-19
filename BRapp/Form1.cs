53dc72b5cee59f287706ec41a29063a64a17ad9a
SvAmstel <simon.van.amstel@gmail.com>
2017-12-19 14:33:44 +0100

    Account call ff op hold, ivm wazige data terug

--- BRapp/Form1.cs
+++ BRapp/Form1.cs
@@ -6,11 +6,13 @@ namespace BRapp
     public partial class Form1 : Form
     {
 
-
+        #region accountID's
         //Account ID Simon
         string accountNummer = "938380520058974208";
         //Account ID Rudi
         //string accountNummer = "937779406217330688";
+        #endregion
+
         ApiCalls apiCall = new ApiCalls();
        
 
