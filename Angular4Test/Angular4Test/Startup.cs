81b67be63b870ae6b6d5bb44f6431a548b82bf3b
SvAmstel <simon.van.amstel@gmail.com>
2017-12-20 16:45:09 +0100

    Angular 4 tutorial

--- Angular4Test/Angular4Test/Startup.cs
+++ Angular4Test/Angular4Test/Startup.cs
@@ -1,15 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using BattleriteAspx.Models;
 
-namespace BattleriteAspx
+namespace Angular4Test
 {
     public class Startup
     {
@@ -24,9 +23,6 @@ namespace BattleriteAspx
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
-            services.AddDbContext<BattleriteAspxContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("BattleriteAspxContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,7 +31,10 @@ namespace BattleriteAspx
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = true
+                });
             }
             else
             {
@@ -49,6 +48,10 @@ namespace BattleriteAspx
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
             });
         }
     }
---
+++ BattleriteAspx/BattleriteAspx/BattleriteAspx/Data/BattleriteAspxContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BattleriteAspx.Models
+{
+    public class BattleriteAspxContext : DbContext
+    {
+        public BattleriteAspxContext (DbContextOptions<BattleriteAspxContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BattleriteAspx.Models.Account> Account { get; set; }
+    }
+}
