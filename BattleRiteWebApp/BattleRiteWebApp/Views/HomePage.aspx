c001b54d6bed5dc594e9520d2311e279c6f614f4
Sugardaddy69 <rudi.jansen@outlook.com>
2017-12-23 20:09:18 +0100

    master page toegevoegd.

---
+++ BattleRiteWebApp/BattleRiteWebApp/Views/HomePage.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/MasterPage.master" AutoEventWireup="true" CodeFile="HomePage.aspx.cs" Inherits="Views_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+
+    <div>
+        <asp:GridView ID="GridView1" HeaderStyle-BackColor="#3AC0F2" HeaderStyle-ForeColor="White"
+            runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="ChampionName" HeaderText="Name" ItemStyle-Width="30" />
+                <asp:BoundField DataField="ChampionBio" HeaderText="Bio" ItemStyle-Width="150" />
+                <asp:BoundField DataField="ChampionImage" HeaderText="Image" ItemStyle-Width="150" />
+            </Columns>
+        </asp:GridView>
+
+    </div>
+
+</asp:Content>
+
+
---
+++ BattleRiteWebApp/BattleRiteWebApp/Views/HomePage.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Views_Default : System.Web.UI.Page
+{
+    private static string playerId = "938380520058974208";
+    private ApiCall apiCall = new ApiCall();
+    private DatabaseConnection databaseConnection = new DatabaseConnection();
+    private string playerName;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        List<Champion> championList = databaseConnection.GetChampions();
+
+        DataTable dt = new DataTable();
+        dt.Columns.AddRange(new DataColumn[3] { new DataColumn("ChampionName", typeof(string)),
+                            new DataColumn("ChampionBio", typeof(string)),
+                            new DataColumn("ChampionImage",typeof(string)) });
+
+        foreach (Champion champion in championList)
+        {
+            dt.Rows.Add(champion.championName, champion.bio, champion.championImage);
+        }
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+    }
+}
