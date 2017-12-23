f9768f84a650ca7566ab0aee86e90ada37299a57
SvAmstel <simon.van.amstel@gmail.com>
2017-12-23 11:55:32 +0100

    Initial commit

---
+++ BattleRiteWebApp/BattleRiteWebApp/App_Code/API/ApiCall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ApiCall
+/// </summary>
+public class ApiCall
+{
+    public ApiCall()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
