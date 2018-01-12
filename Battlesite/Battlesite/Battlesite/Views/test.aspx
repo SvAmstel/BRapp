17691801b8167a9bfa715ef54e7008519f91e5c7
SvAmstel <simon.van.amstel@gmail.com>
2018-01-12 16:42:21 +0100

    Created testbutton on frontend to test DB connection

---
+++ Battlesite/Battlesite/Battlesite/Views/test.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test.aspx.cs" Inherits="Battlesite.Views.test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="testknopje" runat="server" Text="TEST" OnClick="testknopje_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Battlesite/Battlesite/Battlesite/Views/test.aspx.cs
@@ -0,0 +1,26 @@
+﻿using Battlesite.Data;
+using Battlesite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Battlesite.Views
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void testknopje_Click(object sender, EventArgs e)
+        {
+            DatabaseConnection dbconn = new DatabaseConnection();
+            List<Champion> champs = new List<Champion>();
+            champs = dbconn.GetChampions();
+        }
+    }
+}
