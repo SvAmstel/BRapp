8a045c3830169f0458856b1f25232957d73e213f
Sugardaddy69 <rudi.jansen@outlook.com>
2018-01-13 00:22:29 +0100

    test

---
+++ Battlesite/Battlesite/Battlesite/Views/Champions.aspx.cs
@@ -0,0 +1,17 @@
+﻿using Battlesite.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Battlesite.Views
+{
+    public partial class Champions : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { }
+    }
+}
